0fd0619f07f6656e56d6ee3a52f0740c0fd2e91c
marlisapee <mpee@tripadvisor.com>
2024-05-22 13:45:53 -0400

    bug fixes for all

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
 const Todo = sequelize.define('todos', {
-  name: {
+  task: {
     type: DataTypes.STRING,
     allowNull: false,
   },
