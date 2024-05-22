2ba0ec252f697223fe26bf766c0702bcd8ddc35e
marlisapee <mpee@tripadvisor.com>
2024-05-22 11:51:21 -0400

    added relationships to models/index

--- server/models/index.js
+++ server/models/index.js
@@ -1,1 +1,11 @@
-// add relationships
+const User = require('./User');
+const Notes = require('./Notes');
+const Todo = require('./Todo');
+
+User.hasMany(Notes);
+Notes.belongsTo(User);
+
+User.hasMany(Todo);
+Todo.belongsTo(User);
+
+module.exports = { User, Notes, Todo };
