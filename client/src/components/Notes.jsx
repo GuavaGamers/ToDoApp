663aa7b775ce68fea3d9d94a86277049fae0bf02
AndresPaucar7 <nmapfr@gmail.com>
2024-05-23 16:26:53 -0400

    Front-end Notes started

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react'
+import { FaTrash, FaEdit } from 'react-icons/fa';
+import '../Notes.css';
 
 export default function Notes() {
   const [notes, setNotes] = useState([]);
@@ -21,19 +23,48 @@ export default function Notes() {
     fetchNotes();
   },[]);
 
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch(`/api/notes/${id}`, {
+        method: 'DELETE',
+      });
+
+      if(!response.ok) {
+        throw new Error('Failed to delete note');
+      }
+      setNotes(notes.filter((note) => note.id !== id));
+    } catch (error) {
+      console.error("Could not delete note", error)
+    }
+  }
+
   return (
-    <div>Notes
+    <div className='notes'>
+      <h2>Notes</h2>
+      <button className="add-task">Add Task</button>
       {notes.length > 0 ? (
         <ul>
+
           {notes.map(note => (
-            <li key={note.id}>
-              <h3>{note.title}</h3>
-              <p>{note.content}</p>
+            <li key={note.id} className='note-item'>
+              <div className="note-content">
+                <h3>{note.title}</h3>
+                <p>{new Date(note.createdAt).toLocaleString()}</p>
+              </div>
+
+              <div className="note-actions">
+                <button className="edit-button">
+                  <FaEdit />
+                </button>
+                <button className="delete-button" onClick={() => handleDelete(note.id)}>
+                  <FaTrash />
+                </button>
+              </div>
             </li>
           ))}
         </ul>
       ) : (
-        <p>No notes available.</p>
+        <p>No notes are available.</p>
       )}
     </div>
   );
