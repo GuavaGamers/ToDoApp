044e31fe918213b7af1ad8527d324e37b42a7de3
marlisapee <mpee@tripadvisor.com>
2024-05-23 10:52:22 -0400

    organized auth components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import Login from './components/Login';
+import Login from './components/auth/Login';
 import HomePage from './components/HomePage';
 import NavBar from './components/NavBar';
 import User from './components/User';
@@ -10,8 +10,7 @@ import Notes from './components/Notes';
 import Todo from './components/Todo';
 
 import './App.css';
-import SignUp from './components/SignUp';
-import AuthPage from './components/AuthPage';
+import AuthPage from './components/auth/AuthPage';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
@@ -49,7 +48,12 @@ function App() {
             isLoggedIn ? <HomePage /> : <Login handleLogin={handleLogin} />
           }
         />
-        <Route path="/auth" element={<AuthPage handleLogin={handleLogin} />} />
+        <Route
+          path="/auth"
+          element={
+            <AuthPage handleLogin={handleLogin} isLoggedIn={isLoggedIn} />
+          }
+        />
         <Route path="/users" element={<User users={users} />} />
         <Route path="/notes" element={<Notes />} />
         <Route path="/todos" element={<Todo />} />
